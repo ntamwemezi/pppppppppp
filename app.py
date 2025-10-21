66355bf2e1ea3900683bbcd68ec558403a53fd1c
ntamwemezi <ntammanuel0@gmail.com>
2025-10-21 13:13:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 # Load the model
-with open(r'C:\Users\pc\Desktop\Practical_cat\flight_price.pkl', 'rb') as file:
+with open('flight_price.pkl', 'rb') as file:
     My_model = pickle.load(file)
 
 st.title("✈️ Flight Price Predictor")
@@ -34,3 +34,4 @@ if st.button("Predict Price"):
 # if __name__ == '__main__':
  #   main()
    
+
